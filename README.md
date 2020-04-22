5ac32374195c264bff87484f9b265268c0504e83
Jason Puthusseril <jasonputhusseril@ufl.edu>
2020-04-22 11:50:33 -0400

    Updated readme. Removed unnecessary code.

---
+++ README.md
@@ -0,0 +1,42 @@
+# README
+
+Project by: Jason Puthusseril
+Date: 4-22-2020
+
+## About
+
+This project is a basic type inference algorithm for the Haskell language. Written in Prolog, 
+this algorithm provides a type inference mechanism for the basic structures of a functional
+programming language, based on the syntax of Haskell.
+
+
+
+
+## How to Run the Code
+
+* Navigate to the directory with "typeInf.pl" 
+* Start the SWI Prolog Interpreter
+* Load the code by entering
+```bash
+[typeInf].
+```
+* Run unit tests by entering
+```bash
+?- consult("typeInf.plt"), run_tests().
+```
+
+
+
+## Features
+
+1. Basic functions (Arithmetic and Relational operators)
+2. Global variable declarations
+3. Global function declarations
+4. Function calls
+5. "If" statements
+6. "Let in" statements (Local variables)
+7. "Where" statements (Local variables)
+8. Expression computation and code blocks
+
+
+
--- typeInf.pl
+++ typeInf.pl
@@ -1,6 +1,13 @@
+% Author: Jason Puthusseril
+% 4-22-2020
+
+% dynamic predicates
 :- dynamic gvar/2, local/1, lvar/2.
 
-typeExp(X, int) :-
+
+% typeExp propgates types to atoms, variables, and functors
+
+typeExp(X, int) :-                              % Propogating types for atoms
     integer(X).
 
 typeExp(X, float) :-
@@ -10,47 +17,46 @@ typeExp(X, bool) :-
     typeBoolExp(X).
 
 
-
-/* match functions by unifying with arguments 
-    and infering the result
-*/
-typeExp(Fct, T):-
-    \+ var(Fct), /* make sure Fct is not a variable */ 
-    \+ atom(Fct), /* or an atom */
-    functor(Fct, Fname, _Nargs), /* ensure we have a functor */
-    !, /* if we make it here we do not try anything else */
-    Fct =.. [Fname|Args], /* get list of arguments */
-    append(Args, [T], FType), /* make it loook like a function signature */
-    functionType(Fname, TArgs), /* get type of arguments from definition */
-    typeExpList(FType, TArgs). /* recurisvely match types */
+typeExp(Fct, T):-                               % Propogating types for functors (including user defined and built in function)
+    \+ var(Fct),                                /* make sure Fct is not a variable */ 
+    \+ atom(Fct),                               /* or an atom */
+    functor(Fct, Fname, _Nargs),                /* ensure we have a functor */
+    !,                                          /* if we make it here we do not try anything else */
+    Fct =.. [Fname|Args],                       /* get list of arguments */
+    append(Args, [T], FType),                   /* make it loook like a function signature */
+    functionType(Fname, TArgs),                 /* get type of arguments from definition */
+    typeExpList(FType, TArgs).                  /* recurisvely match types */
 
 
-% assert type of global variable in code
-typeExp(Var, T) :-
+typeExp(Var, T) :-                              % assert type of global variable in code
     atom(Var),
     (gvar(Var, T) ; lvar(Var, T)),
     bType(T).
 
 
-/* propagate types */
-typeExp(T, T).
+typeExp(T, T).                                  % propagate types 
 
-/* list version to allow function mathine */
-typeExpList([], []).
+                            
+typeExpList([], []).                            % list version to propogate corresponding lists of atoms/variables/functions
 typeExpList([Hin|Tin], [Hout|Tout]):-
-    typeExp(Hin, Hout), /* type infer the head */
-    typeExpList(Tin, Tout). /* recurse */
+    typeExp(Hin, Hout),                         /* type infer the head */
+    typeExpList(Tin, Tout).                     /* recurse */
 
 
 
+
+% defining types that can be compared
 hasComparison(int).
 hasComparison(float).
 hasComparison(string).
 
+
+% defining types that are numeric
 numericType(int).
 numericType(float).
 
-/* predicate to infer types for boolean expressions */
+
+% predicate to infer types for boolean expressions
 typeBoolExp(true).
 typeBoolExp(false). 
 typeBoolExp( X < Y) :- 
@@ -71,95 +77,98 @@ typeBoolExp( X >= Y) :-
     hasComparison(T).
 
 
-/* TODO: add statements types and their type checking */
 
 
-
-/* global variable definition
-    Example:
-        gvLet(v, T, int) ~ let v = 3;
- */
+% GLOBAL VARIABLE DECLARATION
 typeStatement(gvLet(Name, Code), T):-
-    atom(Name), /* make sure we have a bound name */
-    typeStatement(Code, T), /* infer the type of Code and ensure it is T */
-    bType(T), /* make sure we have an infered type */
-    (local(on) -> asserta(lvar(Name, T)) ; asserta(gvar(Name, T))). /* add definition to database */
+    atom(Name),                                                             % make sure we have a bound name 
+    typeStatement(Code, T),                                                 % infer the type of Code and ensure it is T 
+    bType(T),                                                               % make sure we have an infered type 
+    (local(on) -> asserta(lvar(Name, T)) ; asserta(gvar(Name, T))).         % add definition to database 
 
-/* if statements are encodes as:
-    if(condition:Boolean, trueCode: [Statements], falseCode: [Statements])
-*/
+
+
+% IF STATEMENTS
 typeStatement(if(Cond, TrueB, FalseB), T) :-
-    typeBoolExp(Cond), % Check if Cond is a boolean expression
-    typeCode(TrueB, T), % Check the type of the true block 
-    typeCode(FalseB, T). % asster that the type of the false block is the same as the true blcok
+    typeBoolExp(Cond),                                                      % Check if Cond is a boolean expression
+    typeCode(TrueB, T),                                                     % Check the type of the true block 
+    typeCode(FalseB, T).                                                    % asster that the type of the false block is the same as the true blcok
 
-/* global function should have a name, a list of variables (Vars) with the 
-last one being the return type, a list of statements that make up the function (Code).
-We should assert the type of the last variable in Vars is the same as the the type of the
-last statement in Code 
-*/
+
+
 % GLOBAL FUNCTION
 typeStatement(gFunction(Name, Vars, Code), T) :-
-    atom(Name), % Check that Name is an atom
-    typeStatement(Code, T), % Find the type of the function's code
-    typeCode(Vars, T), % Check that the return type (final type in parameter list) is the same as the function's return type
-    bType(T), % assert that the function's return type is a valid type
-    (local(on) -> asserta(lvar(Name, Vars)) ; asserta(gvar(Name, Vars))). % if local(on), add function to local scope, else add to global
+    atom(Name),                                                             % Check that Name is an atom
+    typeStatement(Code, T),                                                 % Find the type of the function's code
+    typeCode(Vars, T),                                                      % Check that the return type (final type in parameter list) is the same as the function's return type
+    bType(T),                                                               % assert that the function's return type is a valid type
+    (local(on) -> asserta(lvar(Name, Vars)) ; asserta(gvar(Name, Vars))).   % if local(on), add function to local scope, else add to global
+
+
 
 % FUNCTION CALL
 typeStatement(funcCall(Name, Params), T) :-
-    atom(Name), % check that Name is an atom
-    append(Params, [T], FType), % append parameters and return type, aka make it look like a function
-    functionType(Name, Args), % lookup the function
-    typeExpList(Args, FType), % assert that the function call's parameter list and the function signature match
-    typeCode(Args, T). % assert the type of the function call
+    atom(Name),                                                             % check that Name is an atom
+    append(Params, [T], FType),                                             % append parameters and return type, aka make it look like a function
+    functionType(Name, Args),                                               % lookup the function
+    typeExpList(Args, FType),                                               % assert that the function call's parameter list and the function signature match
+    typeCode(Args, T).                                                      % assert the type of the function call
+
+
 
 % LET IN
 typeStatement(letIn(Stmts, Stmt), T) :-
-    deleteLVars(), % delete all local variables
-    asserta(local(on)), % "turn on" / allow saving local variables and functions
-    typeCode(Stmts, _), % evaluate all statements and save their type to lvar (ignore return type)
-    resetLocal(), % "turn off" / disable saving of local variables and function
-    typeStatement(Stmt, T), % Evaluate the type of the "in" statement
-    deleteLVars(). % delete local variables to close scope (and prevent propogation outside of the local scope) 
+    deleteLVars(),                                                          % delete all local variables
+    asserta(local(on)),                                                     % allow saving local variables and functions
+    typeCode(Stmts, _),                                                     % evaluate all statements and save their type to lvar (ignore return type)
+    resetLocal(),                                                           % disable saving of local variables and function
+    typeStatement(Stmt, T),                                                 % Evaluate the type of the "in" statement
+    deleteLVars().                                                          % delete local variables to close scope (and prevent propogation outside of the local scope) 
+
+
 
 % WHERE 
 typeStatement(where(Stmts, WStmts), T) :-
-    deleteLVars(), % delete all local variables
-    asserta(local(on)), % "turn on" / allow saving local variables and functions
-    typeCode(WStmts, _), % evaluate all statements in where block and save type to lvar (ignore return type)
-    resetLocal(), % "turn off" / disable saving of local variables and function
-    typeCode(Stmts, T), % evaluate type of the statements above where block
-    deleteLVars(). % delete local variables to close scope (and prevent propogation outside of the local scope)
+    deleteLVars(),                                                          % delete all local variables
+    asserta(local(on)),                                                     % allow saving local variables and functions
+    typeCode(WStmts, _),                                                    % evaluate all statements in where block and save type to lvar (ignore return type)
+    resetLocal(),                                                           % disable saving of local variables and function
+    typeCode(Stmts, T),                                                     % evaluate type of the statements above where block
+    deleteLVars().                                                          % delete local variables to close scope (and prevent propogation outside of the local scope)
 
 
 
-typeStatement(X, T) :-
-    typeExp(X, T).
+typeStatement(X, T) :-                                                      % expression computation, AKA, allowing atoms/variables/functors
+    typeExp(X, T).                                                          % to be considered as Statements
 
-/* Code is simply a list of statements. The type is 
-    the type of the last statement 
-*/
+
+
+
+% CODE BLOCKS
+% Propogating the type of a list of Statements. The type is the type of the last Statement in the block
 typeCode([S], T):-typeStatement(S, T).
 typeCode([S, S2|Code], T):-
     typeStatement(S,_T),
     typeCode([S2|Code], T).
 
-/* top level function */
+
+
+% Top level function
 infer(Code, T) :-
-    is_list(Code), /* make sure Code is a list */
-    deleteGVars(), /* delete all global definitions */
+    is_list(Code),                                          % make sure Code is a list 
+    deleteGVars(),                                          % delete all global definitions 
     deleteLVars(),
     typeCode(Code, T).
 
-/* Basic types
-    TODO: add more types if needed
- */
+
+
+% BASIC TYPES
 bType(int).
 bType(float).
 bType(string).
 bType(bool).
-bType(unit). /* unit type for things that are not expressions */
+
+
 /*  functions type.
     The type is a list, the last element is the return type
     E.g. add: int->int->int is represented as [int, int, int]
@@ -168,39 +177,19 @@ bType(unit). /* unit type for things that are not expressions */
 bType([H]):- bType(H).
 bType([H|T]):- bType(H), bType(T).
 
-/*
-    TODO: as you encounter global variable definitions
-    or global functions add their definitions to 
-    the database using:
-        asserta( gvar(Name, Type) )
-    To check the types as you encounter them in the code
-    use:
-        gvar(Name, Type) with the Name bound to the name.
-    Type will be bound to the global type
-    Examples:
-        g
 
-    Call the predicate deleveGVars() to delete all global 
-    variables. Best wy to do this is in your top predicate
-*/
 
+
+
+% functions used to clear global and local scopes
 deleteGVars():-retractall(gvar(_, _)), asserta(gvar(_X,_Y):-false()). % deletes all global variables
 deleteLVars():-retractall(lvar(_, _)), asserta(lvar(_X,_Y):-false()). % deletes all local variables
 resetLocal():-retractall(local(_)), asserta(local(off)). % turns off saving of local variables
 
-/*  builtin functions
-    Each definition specifies the name and the 
-    type as a function type
 
-    TODO: add more functions
-*/
 
-/*
 
-iplus :: int -> int -> int
-
-*/
-
+% BUILT IN FUNCTIONS
 fType((+), [T, T, T]) :- numericType(T).
 fType((-), [T, T, T]) :- numericType(T).
 fType((*), [T, T, T]) :- numericType(T).
@@ -209,20 +198,19 @@ fType(fToInt, [float,int]).
 fType(iToFloat, [int,float]).
 fType(print, [_X, unit]). /* simple print */
 
-/* Find function signature
-   A function is either buld in using fType or
-   added as a user definition with gvar(fct, List)
-*/
 
-% Check the user defined functions first
-functionType(Name, Args):-
+
+
+% Finding functions in scope
+functionType(Name, Args):-                      % Check the user defined functions first
     gvar(Name, Args),
-    is_list(Args). % make sure we have a function not a simple variable
+    is_list(Args).                              % make sure we have a function not a simple variable
 
-% Check first built in functions
-functionType(Name, Args) :-
-    fType(Name, Args). % make deterministic
+
+functionType(Name, Args) :-                     % Check first built in functions
+    fType(Name, Args).                          % make deterministic
+
 
 % This gets wiped out but we have it here to make the linter happy
-gvar(_, _) :- false().
-lvar(_, _) :- false().
+% gvar(_, _) :- false().
+% lvar(_, _) :- false().
